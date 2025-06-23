af3fab67dbb7b13faeab8c8219c7ff602ce2d1ce
Haruna <harunahakeem638@gmail.com>
2025-06-23 13:37:07 +0000

    Added the unique algorithm

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -20,5 +20,17 @@ public class Algorithms {
         return maxval;
     }
 
+    public static boolean uniqueElements(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+           for  (int j = i + 1; j < array.length; j++) {
+               if (array[i] == array[j]) {
+                   return false;
+               }
+
+           }
+        }
+        return true;
+    }
+
 }
 
