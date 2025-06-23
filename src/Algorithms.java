da06fa4144e91d7c87d035ca3851fd9cbc4519fb
Haruna <harunahakeem638@gmail.com>
2025-06-23 22:01:54 +0000

    completed matrix multplication, added binary count and n! recursively

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,5 +1,5 @@
 public class Algorithms {
-
+    // Sequential search algorithm
     public static int search(int[] array, int k) {
 
         for (int i = 0; i < array.length; i++) {
@@ -10,6 +10,7 @@ public class Algorithms {
         return -1;
     }
 
+    // Algorithm for finding the max element
     public static int max(int[] array) {
         int maxval = 0;
         for (int i = 0; i < array.length; i++) {
@@ -20,16 +21,72 @@ public class Algorithms {
         return maxval;
     }
 
+    // Algorithm to show if an array contains unique elements
     public static boolean uniqueElement(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-           for  (int j = i + 1; j < array.length; j++) {
-               if (array[i] == array[j]) {
-                   return false;
-               }
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    return false;
+                }
 
-           }
+            }
         }
         return true;
+    }
+
+    // Matrix multiplication algorithm
+    public static double[][] matrixMultiplication(double[][] A, double[][] B) {
+        // check if matrices are compatible for multiplication
+        if (A == null || B == null || A.length == 0 || A[0].length == 0 || B.length == 0 || B[0].length == 0) {
+            throw new IllegalArgumentException("Matrix A and B are null or empty");
+        }
+
+        int n = A.length;
+        if (A[0].length != n || B[0].length != n) {
+            throw new IllegalArgumentException("Matrix A and B must be n-by-n square matrices with same dimensions.");
+        }
+
+        // Initialize the result matrix C with zeros
+        double[][] C = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = 0.0;
+            }
+        }
+
+        // Perform matrix multiplication
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return C;
+    }
+
+    // Algorithm to check the number of binary digits in a positive number's binary representation
+    public static int binary(int n) {
+        // initialize count to 1 since every integer has at least one binary digit
+        int count = 1;
+        while (n > 1) {
+            count += 1;
+
+            n = n / 2;
+
+        }
+        return count;
+    }
+
+    // Algorithm for solving n! recursively
+    public static long F(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return F(n - 1) * n;
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] num = {2,5,6,8,9,1,4,0,7,3};
+        int[] num = {2, 5, 6, 8, 9, 1, 4, 0, 7, 3};
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the number of integer you want to search: ");
@@ -11,9 +11,9 @@ public class Main {
 
         int returned = Algorithms.search(num, k);
         if (returned != -1) {
-            System.out.println("The number is found in Index " + returned );
-        } else  {
-            System.out.println("The number " + returned + " was not found in the array");
+            System.out.println("The number is found in Index " + returned);
+        } else {
+            System.out.println("The number " + k + " was not found in the array");
         }
 
 
@@ -24,10 +24,45 @@ public class Main {
         boolean unique = Algorithms.uniqueElement(num);
         if (unique == true) {
             System.out.println("The numbers are unique");
-        }
-        else {
+        } else {
             System.out.println("The numbers are not unique");
         }
+
+    // matrix multiplication
+        double[][] A = {
+                {1, 2},
+                {3, 4}
+        };
+
+        double[][] B = {
+                {5, 6},
+                {7, 8}
+        };
+
+       try {
+            double[][] resultMatrix = Algorithms.matrixMultiplication(A, B);
+
+            // Print the resulting matrix
+            System.out.println("Result of Matrix Multiplication: ");
+            for (int i = 0; i < resultMatrix.length; i++) {
+                for (int j = 0; j < resultMatrix[i].length; j++) {
+                    System.out.print(resultMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+           System.out.println("An error occurred: " + e.getMessage());
+        }
+
+        // Binary count
+        int n = 10;
+        int digit = Algorithms.binary(n);
+        System.out.println("The number of binary digit of " + n + " is " + digit);
+
+        // n! recursively
+        long factorial = Algorithms.F(n);
+        System.out.println("The factorial of " + n + " is " + factorial);
     }
 
 }
+
